f71ebc0401f9866f4af3bfb3bc8d3e21bf0edc01
Henry Funk <georgehenryfunk@gmail.com>
2020-03-26 16:14:27 -0700

    saving draft .95

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
 'use strict';
+
+var userName = prompt('What is your name?');
+console.log(userName + ' is the answer to \'What is your name?\'');
+alert('Hi ' + userName + '! Welcome to my \'About Me\' website');
