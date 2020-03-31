82d8a03c4b76ee6de6ee68b3783cb034da896b22
Henry Funk <georgehenryfunk@gmail.com>
2020-03-30 18:48:42 -0700

    figuring out while and for loops, up to question 6 from Lab 2

--- js/app.js
+++ js/app.js
@@ -4,63 +4,80 @@ var userName = prompt('What is your name?');
 // console.log(userName + ' is the answer to \'What is your name?\'');
 alert('Hi ' + userName + '! Welcome to my \'About Me\' website');
 
-// var guessingGame = prompt('Would you like to learn more about me, ' + userName + '?').trim().toLowerCase();
-// // console.log('asking ' + userName + ' if they want to play my guessing game');
-// if((guessingGame === 'yes') || (guessingGame === 'y')){
-//     alert('Great, let\'s see if you can guess a few fun facts about me, ' + userName);
-// }
-// else if((guessingGame === 'no') || (guessingGame === 'n')){
-//     alert('Too bad, ' + userName + '. My site, my rules. You\'re going to try and guess a few fun facts about me whether you like it or not!');
-// } else {
-//     // while((((guessingGame !== 'yes') || (guessingGame !== 'y') || (guessingGame !== 'no') || (guessingGame !== 'n'))) {
+var guessingGame = prompt('Would you like to learn more about me, ' + userName + '?').trim().toLowerCase();
+// console.log('asking ' + userName + ' if they want to play my guessing game');
+while((guessingGame !== 'yes') && (guessingGame !== 'no')){
+  guessingGame = prompt('Please answer \'yes\' or \'no\'').trim().toLowerCase();
+}
+if((guessingGame === 'yes') || (guessingGame === 'y')){
+  alert('Great, let\'s see if you can guess a few fun facts about me, ' + userName);
+}
+else if((guessingGame === 'no') || (guessingGame === 'n')){
+  alert('Too bad, ' + userName + '. My site, my rules. You\'re going to try and guess a few fun facts about me whether you like it or not!');
+}
 
-//     // }{
-//     alert('Please answer \'Yes\' or \'No\' ' + userName);
-// }
+//   // while((((guessingGame !== 'yes') || (guessingGame !== 'y') || (guessingGame !== 'no') || (guessingGame !== 'n'))) {
 
-// var age = prompt('Do you think I\'m 30 years old, ' + userName + '?').trim().toLowerCase();
-// // console.log('asking ' + userName + ' if they think I\'m 30 years old');
-// if((age === 'yes') || (age === 'y')){
-//     alert('You got it, ' + userName);
-// }
-// else if((age === 'no') || (age === 'n')){
-//     alert('Not quite, ' + userName + '. I am indeed 30! Better luck on this next one.');
-// } else {
-//     alert('Please answer \'Yes\' or \'No\' ' + userName);
+//   // }{
+//   alert('Please answer \'Yes\' or \'No\' ' + userName);
 // }
 
-// var tv = prompt('Do you think I\'ve ever been on national Japanese TV, ' + userName + '?').trim().toLowerCase();
-// // console.log('asking ' + userName + ' if they think I\'ve ever been on Japanese TV');
-// if((tv === 'yes') || (tv === 'y')){
-//     alert('You got it, ' + userName + '! I was talking about Kentucky Fried Chicken when I studied abroad there back in 2010... Long story');
-// }
-// else if((tv === 'no') || (tv === 'n')){
-//     alert('Believe it or not, ' + userName + '. I was on Japanese TV! Weird times.');
-// } else {
-//     alert('Please answer \'Yes\' or \'No\' ' + userName);
-// }
+var age = prompt('Do you think I\'m 30 years old, ' + userName + '?').trim().toLowerCase();
+// console.log('asking ' + userName + ' if they think I\'m 30 years old');
+while(age !== 'yes' && age !== 'no'){
+  age = prompt('Please answer \'yes\' or \'no\'').trim().toLowerCase();
+}
+if((age === 'yes') || (age === 'y')){
+  alert('You got it, ' + userName);
+}
+else if((age === 'no') || (age === 'n')){
+  alert('Not quite, ' + userName + '. I am indeed 30! Better luck on this next one.');
+}
 
-// var pct = prompt('Do you think I\'ve ever hiked from Mexico to Canada, ' + userName + '?').trim().toLowerCase();
-// // console.log('asking ' + userName + ' if they think I\'ve ever hiked across the country');
-// if((pct === 'yes') || (pct === 'y')){
-//     alert('You got it, ' + userName + '! I hiked the PCT in 2017! Not the best year to try a thru-hike... had to skip about 500 miles due to fire.');
-// }
-// else if((pct === 'no') || (pct === 'n')){
-//     alert('Well, ' + userName + ', you\'re kind of right on this one. I had to skip 500 miles due to fire, but I still hiked 2,100 miles of the PCT in 2017!');
 // } else {
-//     alert('Please answer \'Yes\' or \'No\' ' + userName);
+//   alert('Please answer \'Yes\' or \'No\' ' + userName);
 // }
+var tv = prompt('Do you think I\'ve ever been on national Japanese TV, ' + userName + '?').trim().toLowerCase();
+// console.log('asking ' + userName + ' if they think I\'ve ever been on Japanese TV');
+while(tv !== 'yes' && tv !== 'no'){
+  tv = prompt('Please answer yes or no!').trim().toLowerCase();
+}
+if((tv === 'yes') || (tv === 'y')){
+  alert('You got it, ' + userName + '! I was talking about Kentucky Fried Chicken when I studied abroad there back in 2010... Long story');
+}
+else if((tv === 'no') || (tv === 'n')){
+  alert('Believe it or not, ' + userName + '. I was on Japanese TV! Weird times.');
+} else {
+  alert('Please answer \'Yes\' or \'No\' ' + userName);
+}
 
-// var tp = prompt('Do you think I insert a fresh roll of TP waterfall style, ' + userName + '?').trim().toLowerCase();
-// // console.log('asking ' + userName + ' if I know how to properly install a fresh roll of TP');
-// if((tp === 'yes') || (tp === 'y')){
-//     alert('You\'re damn right I do, ' + userName + '! I\'m not an animal');
-// }
-// else if((tp === 'no') || (tp === 'n')){
-//     alert('What kind of person do you think I am, ' + userName + '? I\'m not an animal');
-// } else {
-//     alert('Please answer \'Yes\' or \'No\' ' + userName);
-// }
+var pct = prompt('Do you think I\'ve ever hiked from Mexico to Canada, ' + userName + '?').trim().toLowerCase();
+// console.log('asking ' + userName + ' if they think I\'ve ever hiked across the country');
+while(pct !== 'yes' && pct !== 'no'){
+  pct = prompt('Please answer yes or no!').trim().toLowerCase();
+}
+if((pct === 'yes') || (pct === 'y')){
+  alert('You got it, ' + userName + '! I hiked the PCT in 2017! Not the best year to try a thru-hike... had to skip about 500 miles due to fire.');
+}
+else if((pct === 'no') || (pct === 'n')){
+  alert('Well, ' + userName + ', you\'re kind of right on this one. I had to skip 500 miles due to fire, but I still hiked 2,100 miles of the PCT in 2017!');
+} else {
+  alert('Please answer \'Yes\' or \'No\' ' + userName);
+}
+
+var tp = prompt('Do you think I insert a fresh roll of TP waterfall style, ' + userName + '?').trim().toLowerCase();
+// console.log('asking ' + userName + ' if I know how to properly install a fresh roll of TP');
+while(tp !== 'yes' && tp !== 'no'){
+  tp = prompt('Please answer yes or no!').trim().toLowerCase();
+}
+if((tp === 'yes') || (tp === 'y')){
+  alert('You\'re damn right I do, ' + userName + '! I\'m not an animal');
+}
+else if((tp === 'no') || (tp === 'n')){
+  alert('What kind of person do you think I am, ' + userName + '? I\'m not an animal');
+} else {
+  alert('Please answer \'Yes\' or \'No\' ' + userName);
+}
 
 
 // while(isNaN(siblings)){
@@ -69,22 +86,26 @@ alert('Hi ' + userName + '! Welcome to my \'About Me\' website');
 
 console.log('asking ' + userName + ' how many siblings they think I have');
 
-for(var i = 0; i < 4; i++){
-  console.log(i);
-  var siblings = prompt('How many siblings do you think I have?').trim();
-  if(i >= 3){
-    alert('Correct answer is 0. I\'m an only child, can\'t you tell??');
+var siblings = prompt('How many siblings do you think I have?').trim();
+while(isNaN(siblings)){
+  alert('Answer must be a number!');
+  for(var i = 0; i < 4; i++){
+    console.log(i);
+    siblings = prompt('How many siblings do you think I have?').trim();
+    if(i >= 3){
+      alert('Correct answer is 0. I\'m an only child, can\'t you tell??');
+    }
+    else if(siblings > 0){
+      alert('That\'s too high ' + userName + '. Guess again.');
+    }
+    else if(siblings === '0'){
+      alert('You got it, ' + userName + '!');
+      break;
+    }
   }
-  else if(siblings > 0){
-    alert('That\'s too high ' + userName + '. Guess again.');
-  }
-  else if(siblings === '0'){
-    alert('You got it, ' + userName + '!');
-    break;
-  }
-  else if(siblings !== isNaN){
-    alert('Please answer with a number');
-  }
+//   else if(siblings !== isNaN){
+//     alert('Please answer with a number');
+//   }
 }
 
 // QUESTION 7 - using arrays for multiple correct answers
@@ -92,18 +113,18 @@ for(var i = 0; i < 4; i++){
 var faveFruits = ['persimmon', 'avocado', 'grapes'];
 // console.log(faveFuits);
 
-for(var i = 0; i < 6; i++){
-    var faveFruitsQ = prompt('Can you guess one of my 3 favorite fruits?').trim().toLocaleLowerCase();
-    if(i >=5){
-        alert('Correct answers are: 1) Persimmon 2) Avocado 3) Grapes');
-    }
-    else  if((faveFruitsQ === faveFruits[0]) || (faveFruitsQ === faveFruits[1]) || (faveFruitsQ === faveFruits[2])){
-        alert('Yes! You guessed one of my 3 faves! Persimmon, avocados, and grapes are the best!');
-        break;
-    }
-    else if(faveFruitsQ !== faveFruits){
-        alert('Not bad, but not one of my top 3! Guess again');
-    }
+for(i = 0; i < 6; i++){
+  var faveFruitsQ = prompt('Can you guess one of my 3 favorite fruits?').trim().toLocaleLowerCase();
+  if(i >=5){
+    alert('Correct answers are: 1) Persimmon 2) Avocado 3) Grapes');
+  }
+  else if((faveFruitsQ === faveFruits[0]) || (faveFruitsQ === faveFruits[1]) || (faveFruitsQ === faveFruits[2])){
+    alert('Yes! You guessed one of my 3 faves! Persimmon, avocados, and grapes are the best!');
+    break;
+  }
+  else if(faveFruitsQ !== faveFruits){
+    alert('Not bad, but not one of my top 3! Guess again');
+  }
 }
 
-alert('Thank you for playing my guessing game, ' + userName + '! As a reward, you can see my site now. Enjoy!')
+alert('Thank you for playing my guessing game, ' + userName + '! As a reward, you can see my site now. Enjoy!');
